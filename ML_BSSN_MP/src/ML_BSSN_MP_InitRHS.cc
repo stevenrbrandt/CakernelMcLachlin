621105dc2c1c4302fa103dd9cdf887907dbe38b8
Ian Hinder <ian.hinder@aei.mpg.de>
2011-06-13 10:56:21 +0200

    Regenerate code
    Test ML_BSSN_sgw3d_rhs fails with differences in B, Brhs and betarhs because the initial data for these has been modified.  Tests ML_BSSN_sgw3d and ML_BSSN_O8_sgw3d_rhs fail in all variables due to coupling.

---
+++ ML_BSSN_MP/src/ML_BSSN_MP_InitRHS.cc
@@ -0,0 +1,240 @@
+/*  File produced by Kranc */
+
+#define KRANC_C
+
+#include <assert.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "cctk.h"
+#include "cctk_Arguments.h"
+#include "cctk_Parameters.h"
+#include "GenericFD.h"
+#include "Differencing.h"
+#include "loopcontrol.h"
+
+/* Define macros used in calculations */
+#define INITVALUE (42)
+#define QAD(x) (SQR(SQR(x)))
+#define INV(x) ((1.0) / (x))
+#define SQR(x) ((x) * (x))
+#define CUB(x) ((x) * (x) * (x))
+
+static void ML_BSSN_MP_InitRHS_Body(cGH const * restrict const cctkGH, int const dir, int const face, CCTK_REAL const normal[3], CCTK_REAL const tangentA[3], CCTK_REAL const tangentB[3], int const min[3], int const max[3], int const n_subblock_gfs, CCTK_REAL * restrict const subblock_gfs[])
+{
+  DECLARE_CCTK_ARGUMENTS;
+  DECLARE_CCTK_PARAMETERS;
+  
+  
+  /* Declare finite differencing variables */
+  
+  if (verbose > 1)
+  {
+    CCTK_VInfo(CCTK_THORNSTRING,"Entering ML_BSSN_MP_InitRHS_Body");
+  }
+  
+  if (cctk_iteration % ML_BSSN_MP_InitRHS_calc_every != ML_BSSN_MP_InitRHS_calc_offset)
+  {
+    return;
+  }
+  
+  const char *groups[] = {"ML_BSSN_MP::ML_curvrhs","ML_BSSN_MP::ML_dtlapserhs","ML_BSSN_MP::ML_dtshiftrhs","ML_BSSN_MP::ML_Gammarhs","ML_BSSN_MP::ML_lapserhs","ML_BSSN_MP::ML_log_confacrhs","ML_BSSN_MP::ML_metricrhs","ML_BSSN_MP::ML_shiftrhs","ML_BSSN_MP::ML_trace_curvrhs"};
+  GenericFD_AssertGroupStorage(cctkGH, "ML_BSSN_MP_InitRHS", 9, groups);
+  
+  
+  /* Include user-supplied include files */
+  
+  /* Initialise finite differencing variables */
+  ptrdiff_t const di = 1;
+  ptrdiff_t const dj = CCTK_GFINDEX3D(cctkGH,0,1,0) - CCTK_GFINDEX3D(cctkGH,0,0,0);
+  ptrdiff_t const dk = CCTK_GFINDEX3D(cctkGH,0,0,1) - CCTK_GFINDEX3D(cctkGH,0,0,0);
+  ptrdiff_t const cdi = sizeof(CCTK_REAL) * di;
+  ptrdiff_t const cdj = sizeof(CCTK_REAL) * dj;
+  ptrdiff_t const cdk = sizeof(CCTK_REAL) * dk;
+  CCTK_REAL const dx = ToReal(CCTK_DELTA_SPACE(0));
+  CCTK_REAL const dy = ToReal(CCTK_DELTA_SPACE(1));
+  CCTK_REAL const dz = ToReal(CCTK_DELTA_SPACE(2));
+  CCTK_REAL const dt = ToReal(CCTK_DELTA_TIME);
+  CCTK_REAL const dxi = INV(dx);
+  CCTK_REAL const dyi = INV(dy);
+  CCTK_REAL const dzi = INV(dz);
+  CCTK_REAL const khalf = 0.5;
+  CCTK_REAL const kthird = 1/3.0;
+  CCTK_REAL const ktwothird = 2.0/3.0;
+  CCTK_REAL const kfourthird = 4.0/3.0;
+  CCTK_REAL const keightthird = 8.0/3.0;
+  CCTK_REAL const hdxi = 0.5 * dxi;
+  CCTK_REAL const hdyi = 0.5 * dyi;
+  CCTK_REAL const hdzi = 0.5 * dzi;
+  
+  /* Initialize predefined quantities */
+  CCTK_REAL const p1o12dx = 0.0833333333333333333333333333333*INV(dx);
+  CCTK_REAL const p1o12dy = 0.0833333333333333333333333333333*INV(dy);
+  CCTK_REAL const p1o12dz = 0.0833333333333333333333333333333*INV(dz);
+  CCTK_REAL const p1o144dxdy = 0.00694444444444444444444444444444*INV(dx)*INV(dy);
+  CCTK_REAL const p1o144dxdz = 0.00694444444444444444444444444444*INV(dx)*INV(dz);
+  CCTK_REAL const p1o144dydz = 0.00694444444444444444444444444444*INV(dy)*INV(dz);
+  CCTK_REAL const p1o24dx = 0.0416666666666666666666666666667*INV(dx);
+  CCTK_REAL const p1o24dy = 0.0416666666666666666666666666667*INV(dy);
+  CCTK_REAL const p1o24dz = 0.0416666666666666666666666666667*INV(dz);
+  CCTK_REAL const p1o64dx = 0.015625*INV(dx);
+  CCTK_REAL const p1o64dy = 0.015625*INV(dy);
+  CCTK_REAL const p1o64dz = 0.015625*INV(dz);
+  CCTK_REAL const p1odx = INV(dx);
+  CCTK_REAL const p1ody = INV(dy);
+  CCTK_REAL const p1odz = INV(dz);
+  CCTK_REAL const pm1o12dx2 = -0.0833333333333333333333333333333*INV(SQR(dx));
+  CCTK_REAL const pm1o12dy2 = -0.0833333333333333333333333333333*INV(SQR(dy));
+  CCTK_REAL const pm1o12dz2 = -0.0833333333333333333333333333333*INV(SQR(dz));
+  
+  /* Jacobian variable pointers */
+  bool const use_jacobian = (!CCTK_IsFunctionAliased("MultiPatch_GetMap") || MultiPatch_GetMap(cctkGH) != jacobian_identity_map)
+                       && strlen(jacobian_group) > 0;
+  if (use_jacobian && strlen(jacobian_derivative_group) == 0)
+  {
+    CCTK_WARN (1, "GenericFD::jacobian_group and GenericFD::jacobian_derivative_group must both be set to valid group names");
+  }
+  
+  CCTK_REAL const *restrict jacobian_ptrs[9];
+  if (use_jacobian) GenericFD_GroupDataPointers(cctkGH, jacobian_group,
+                                                9, jacobian_ptrs);
+  
+  CCTK_REAL const *restrict const J11 = use_jacobian ? jacobian_ptrs[0] : 0;
+  CCTK_REAL const *restrict const J12 = use_jacobian ? jacobian_ptrs[1] : 0;
+  CCTK_REAL const *restrict const J13 = use_jacobian ? jacobian_ptrs[2] : 0;
+  CCTK_REAL const *restrict const J21 = use_jacobian ? jacobian_ptrs[3] : 0;
+  CCTK_REAL const *restrict const J22 = use_jacobian ? jacobian_ptrs[4] : 0;
+  CCTK_REAL const *restrict const J23 = use_jacobian ? jacobian_ptrs[5] : 0;
+  CCTK_REAL const *restrict const J31 = use_jacobian ? jacobian_ptrs[6] : 0;
+  CCTK_REAL const *restrict const J32 = use_jacobian ? jacobian_ptrs[7] : 0;
+  CCTK_REAL const *restrict const J33 = use_jacobian ? jacobian_ptrs[8] : 0;
+  
+  CCTK_REAL const *restrict jacobian_derivative_ptrs[18];
+  if (use_jacobian) GenericFD_GroupDataPointers(cctkGH, jacobian_derivative_group,
+                                                18, jacobian_derivative_ptrs);
+  
+  CCTK_REAL const *restrict const dJ111 = use_jacobian ? jacobian_derivative_ptrs[0] : 0;
+  CCTK_REAL const *restrict const dJ112 = use_jacobian ? jacobian_derivative_ptrs[1] : 0;
+  CCTK_REAL const *restrict const dJ113 = use_jacobian ? jacobian_derivative_ptrs[2] : 0;
+  CCTK_REAL const *restrict const dJ122 = use_jacobian ? jacobian_derivative_ptrs[3] : 0;
+  CCTK_REAL const *restrict const dJ123 = use_jacobian ? jacobian_derivative_ptrs[4] : 0;
+  CCTK_REAL const *restrict const dJ133 = use_jacobian ? jacobian_derivative_ptrs[5] : 0;
+  CCTK_REAL const *restrict const dJ211 = use_jacobian ? jacobian_derivative_ptrs[6] : 0;
+  CCTK_REAL const *restrict const dJ212 = use_jacobian ? jacobian_derivative_ptrs[7] : 0;
+  CCTK_REAL const *restrict const dJ213 = use_jacobian ? jacobian_derivative_ptrs[8] : 0;
+  CCTK_REAL const *restrict const dJ222 = use_jacobian ? jacobian_derivative_ptrs[9] : 0;
+  CCTK_REAL const *restrict const dJ223 = use_jacobian ? jacobian_derivative_ptrs[10] : 0;
+  CCTK_REAL const *restrict const dJ233 = use_jacobian ? jacobian_derivative_ptrs[11] : 0;
+  CCTK_REAL const *restrict const dJ311 = use_jacobian ? jacobian_derivative_ptrs[12] : 0;
+  CCTK_REAL const *restrict const dJ312 = use_jacobian ? jacobian_derivative_ptrs[13] : 0;
+  CCTK_REAL const *restrict const dJ313 = use_jacobian ? jacobian_derivative_ptrs[14] : 0;
+  CCTK_REAL const *restrict const dJ322 = use_jacobian ? jacobian_derivative_ptrs[15] : 0;
+  CCTK_REAL const *restrict const dJ323 = use_jacobian ? jacobian_derivative_ptrs[16] : 0;
+  CCTK_REAL const *restrict const dJ333 = use_jacobian ? jacobian_derivative_ptrs[17] : 0;
+  
+  /* Loop over the grid points */
+  #pragma omp parallel
+  LC_LOOP3 (ML_BSSN_MP_InitRHS,
+    i,j,k, min[0],min[1],min[2], max[0],max[1],max[2],
+    cctk_lsh[0],cctk_lsh[1],cctk_lsh[2])
+  {
+    ptrdiff_t const index = di*i + dj*j + dk*k;
+    
+    /* Assign local copies of grid functions */
+    
+    
+    
+    
+    /* Include user supplied include files */
+    
+    /* Precompute derivatives */
+    
+    /* Calculate temporaries and grid functions */
+    CCTK_REAL phirhsL = 0;
+    
+    CCTK_REAL gt11rhsL = 0;
+    
+    CCTK_REAL gt12rhsL = 0;
+    
+    CCTK_REAL gt13rhsL = 0;
+    
+    CCTK_REAL gt22rhsL = 0;
+    
+    CCTK_REAL gt23rhsL = 0;
+    
+    CCTK_REAL gt33rhsL = 0;
+    
+    CCTK_REAL trKrhsL = 0;
+    
+    CCTK_REAL At11rhsL = 0;
+    
+    CCTK_REAL At12rhsL = 0;
+    
+    CCTK_REAL At13rhsL = 0;
+    
+    CCTK_REAL At22rhsL = 0;
+    
+    CCTK_REAL At23rhsL = 0;
+    
+    CCTK_REAL At33rhsL = 0;
+    
+    CCTK_REAL Xt1rhsL = 0;
+    
+    CCTK_REAL Xt2rhsL = 0;
+    
+    CCTK_REAL Xt3rhsL = 0;
+    
+    CCTK_REAL alpharhsL = 0;
+    
+    CCTK_REAL ArhsL = 0;
+    
+    CCTK_REAL beta1rhsL = 0;
+    
+    CCTK_REAL beta2rhsL = 0;
+    
+    CCTK_REAL beta3rhsL = 0;
+    
+    CCTK_REAL B1rhsL = 0;
+    
+    CCTK_REAL B2rhsL = 0;
+    
+    CCTK_REAL B3rhsL = 0;
+    
+    /* Copy local copies back to grid functions */
+    alpharhs[index] = alpharhsL;
+    Arhs[index] = ArhsL;
+    At11rhs[index] = At11rhsL;
+    At12rhs[index] = At12rhsL;
+    At13rhs[index] = At13rhsL;
+    At22rhs[index] = At22rhsL;
+    At23rhs[index] = At23rhsL;
+    At33rhs[index] = At33rhsL;
+    B1rhs[index] = B1rhsL;
+    B2rhs[index] = B2rhsL;
+    B3rhs[index] = B3rhsL;
+    beta1rhs[index] = beta1rhsL;
+    beta2rhs[index] = beta2rhsL;
+    beta3rhs[index] = beta3rhsL;
+    gt11rhs[index] = gt11rhsL;
+    gt12rhs[index] = gt12rhsL;
+    gt13rhs[index] = gt13rhsL;
+    gt22rhs[index] = gt22rhsL;
+    gt23rhs[index] = gt23rhsL;
+    gt33rhs[index] = gt33rhsL;
+    phirhs[index] = phirhsL;
+    trKrhs[index] = trKrhsL;
+    Xt1rhs[index] = Xt1rhsL;
+    Xt2rhs[index] = Xt2rhsL;
+    Xt3rhs[index] = Xt3rhsL;
+  }
+  LC_ENDLOOP3 (ML_BSSN_MP_InitRHS);
+}
+
+extern "C" void ML_BSSN_MP_InitRHS(CCTK_ARGUMENTS)
+{
+  DECLARE_CCTK_ARGUMENTS;
+  DECLARE_CCTK_PARAMETERS;
+  
+  GenericFD_LoopOverEverything(cctkGH, &ML_BSSN_MP_InitRHS_Body);
+}
