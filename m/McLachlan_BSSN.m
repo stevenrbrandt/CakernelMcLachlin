56cea2adf29ca0223b53581313841c86ca8b5ac9
Ian Hinder <ian.hinder@aei.mpg.de>
2012-04-03 16:01:55 +0200

    McLachlan_BSSN.m: Compute all derivatives in RHS calculation in separate calculations and gridfunctions

--- m/McLachlan_BSSN.m
+++ m/McLachlan_BSSN.m
@@ -514,7 +514,7 @@ evolCalc =
                  rho, S[la], trS, fac1, fac2, dottrK, dotXt[ua], epsdiss[ua]},
   SplitBy -> {{dot[trK], dot[phi], dot[gt[la,lb]], dot[Xt[ui]], dot[alpha], dot[A], dot[beta[ua]], dot[B[ua]]},
               {dot[At[la,lb]]}},
-  SeparatedDerivatives -> {PD[_,i_,j_] /; i =!= j},
+  SeparatedDerivatives -> {PD[_,__]},
   Equations -> 
   {
     dir[ua] -> Sign[beta[ua]],
