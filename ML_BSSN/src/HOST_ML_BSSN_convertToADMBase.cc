ed13373bca05d3f63b25618f716f10f0ccecba9d
Ian Hinder <ian.hinder@aei.mpg.de>
2012-04-30 21:23:51 +0200

    Regenerate code

---
+++ ML_BSSN/src/HOST_ML_BSSN_convertToADMBase.cc
@@ -0,0 +1,224 @@
+/*  File produced by Kranc */
+
+#define KRANC_C
+
+#include <assert.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "cctk.h"
+#include "cctk_Arguments.h"
+#include "cctk_Parameters.h"
+#include "GenericFD.h"
+#include "Differencing.h"
+#include "cctk_Loop.h"
+#include "loopcontrol.h"
+
+/* Define macros used in calculations */
+#define INITVALUE (42)
+#define QAD(x) (SQR(SQR(x)))
+#define INV(x) ((1.0) / (x))
+#define SQR(x) ((x) * (x))
+#define CUB(x) ((x) * (x) * (x))
+
+static void HOST_ML_BSSN_convertToADMBase_Body(cGH const * restrict const cctkGH, int const dir, int const face, CCTK_REAL const normal[3], CCTK_REAL const tangentA[3], CCTK_REAL const tangentB[3], int const imin[3], int const imax[3], int const n_subblock_gfs, CCTK_REAL * restrict const subblock_gfs[])
+{
+  DECLARE_CCTK_ARGUMENTS;
+  DECLARE_CCTK_PARAMETERS;
+  
+  
+  /* Include user-supplied include files */
+  
+  /* Initialise finite differencing variables */
+  ptrdiff_t const di = 1;
+  ptrdiff_t const dj = CCTK_GFINDEX3D(cctkGH,0,1,0) - CCTK_GFINDEX3D(cctkGH,0,0,0);
+  ptrdiff_t const dk = CCTK_GFINDEX3D(cctkGH,0,0,1) - CCTK_GFINDEX3D(cctkGH,0,0,0);
+  ptrdiff_t const cdi = sizeof(CCTK_REAL) * di;
+  ptrdiff_t const cdj = sizeof(CCTK_REAL) * dj;
+  ptrdiff_t const cdk = sizeof(CCTK_REAL) * dk;
+  CCTK_REAL const dx = ToReal(CCTK_DELTA_SPACE(0));
+  CCTK_REAL const dy = ToReal(CCTK_DELTA_SPACE(1));
+  CCTK_REAL const dz = ToReal(CCTK_DELTA_SPACE(2));
+  CCTK_REAL const dt = ToReal(CCTK_DELTA_TIME);
+  CCTK_REAL const t = ToReal(cctk_time);
+  CCTK_REAL const dxi = INV(dx);
+  CCTK_REAL const dyi = INV(dy);
+  CCTK_REAL const dzi = INV(dz);
+  CCTK_REAL const khalf = 0.5;
+  CCTK_REAL const kthird = 1/3.0;
+  CCTK_REAL const ktwothird = 2.0/3.0;
+  CCTK_REAL const kfourthird = 4.0/3.0;
+  CCTK_REAL const keightthird = 8.0/3.0;
+  CCTK_REAL const hdxi = 0.5 * dxi;
+  CCTK_REAL const hdyi = 0.5 * dyi;
+  CCTK_REAL const hdzi = 0.5 * dzi;
+  
+  /* Initialize predefined quantities */
+  CCTK_REAL const p1o1024dx = 0.0009765625*INV(dx);
+  CCTK_REAL const p1o1024dy = 0.0009765625*INV(dy);
+  CCTK_REAL const p1o1024dz = 0.0009765625*INV(dz);
+  CCTK_REAL const p1o1680dx = 0.000595238095238095238095238095238*INV(dx);
+  CCTK_REAL const p1o1680dy = 0.000595238095238095238095238095238*INV(dy);
+  CCTK_REAL const p1o1680dz = 0.000595238095238095238095238095238*INV(dz);
+  CCTK_REAL const p1o5040dx2 = 0.000198412698412698412698412698413*INV(SQR(dx));
+  CCTK_REAL const p1o5040dy2 = 0.000198412698412698412698412698413*INV(SQR(dy));
+  CCTK_REAL const p1o5040dz2 = 0.000198412698412698412698412698413*INV(SQR(dz));
+  CCTK_REAL const p1o560dx = 0.00178571428571428571428571428571*INV(dx);
+  CCTK_REAL const p1o560dy = 0.00178571428571428571428571428571*INV(dy);
+  CCTK_REAL const p1o560dz = 0.00178571428571428571428571428571*INV(dz);
+  CCTK_REAL const p1o705600dxdy = 1.41723356009070294784580498866e-6*INV(dx*dy);
+  CCTK_REAL const p1o705600dxdz = 1.41723356009070294784580498866e-6*INV(dx*dz);
+  CCTK_REAL const p1o705600dydz = 1.41723356009070294784580498866e-6*INV(dy*dz);
+  CCTK_REAL const p1o840dx = 0.00119047619047619047619047619048*INV(dx);
+  CCTK_REAL const p1o840dy = 0.00119047619047619047619047619048*INV(dy);
+  CCTK_REAL const p1o840dz = 0.00119047619047619047619047619048*INV(dz);
+  CCTK_REAL const p1odx = INV(dx);
+  CCTK_REAL const p1ody = INV(dy);
+  CCTK_REAL const p1odz = INV(dz);
+  CCTK_REAL const pm1o840dx = -0.00119047619047619047619047619048*INV(dx);
+  CCTK_REAL const pm1o840dy = -0.00119047619047619047619047619048*INV(dy);
+  CCTK_REAL const pm1o840dz = -0.00119047619047619047619047619048*INV(dz);
+  
+  /* Assign local copies of arrays functions */
+  
+  
+  
+  /* Calculate temporaries and arrays functions */
+  
+  /* Copy local copies back to grid functions */
+  
+  /* Loop over the grid points */
+  #pragma omp parallel
+  CCTK_LOOP3(HOST_ML_BSSN_convertToADMBase,
+    i,j,k, imin[0],imin[1],imin[2], imax[0],imax[1],imax[2],
+    cctk_lsh[0],cctk_lsh[1],cctk_lsh[2])
+  {
+    ptrdiff_t const index = di*i + dj*j + dk*k;
+    
+    /* Assign local copies of grid functions */
+    
+    CCTK_REAL alphaL = alpha[index];
+    CCTK_REAL At11L = At11[index];
+    CCTK_REAL At12L = At12[index];
+    CCTK_REAL At13L = At13[index];
+    CCTK_REAL At22L = At22[index];
+    CCTK_REAL At23L = At23[index];
+    CCTK_REAL At33L = At33[index];
+    CCTK_REAL beta1L = beta1[index];
+    CCTK_REAL beta2L = beta2[index];
+    CCTK_REAL beta3L = beta3[index];
+    CCTK_REAL gt11L = gt11[index];
+    CCTK_REAL gt12L = gt12[index];
+    CCTK_REAL gt13L = gt13[index];
+    CCTK_REAL gt22L = gt22[index];
+    CCTK_REAL gt23L = gt23[index];
+    CCTK_REAL gt33L = gt33[index];
+    CCTK_REAL phiL = phi[index];
+    CCTK_REAL trKL = trK[index];
+    
+    
+    /* Include user supplied include files */
+    
+    /* Precompute derivatives */
+    
+    /* Calculate temporaries and grid functions */
+    CCTK_REAL e4phi = 
+      IfThen(conformalMethod,INV(SQR(phiL)),exp(4*phiL));
+    
+    CCTK_REAL gxxL = gt11L*e4phi;
+    
+    CCTK_REAL gxyL = gt12L*e4phi;
+    
+    CCTK_REAL gxzL = gt13L*e4phi;
+    
+    CCTK_REAL gyyL = gt22L*e4phi;
+    
+    CCTK_REAL gyzL = gt23L*e4phi;
+    
+    CCTK_REAL gzzL = gt33L*e4phi;
+    
+    CCTK_REAL kxxL = (At11L + 
+      0.333333333333333333333333333333*gt11L*trKL)*e4phi;
+    
+    CCTK_REAL kxyL = (At12L + 
+      0.333333333333333333333333333333*gt12L*trKL)*e4phi;
+    
+    CCTK_REAL kxzL = (At13L + 
+      0.333333333333333333333333333333*gt13L*trKL)*e4phi;
+    
+    CCTK_REAL kyyL = (At22L + 
+      0.333333333333333333333333333333*gt22L*trKL)*e4phi;
+    
+    CCTK_REAL kyzL = (At23L + 
+      0.333333333333333333333333333333*gt23L*trKL)*e4phi;
+    
+    CCTK_REAL kzzL = (At33L + 
+      0.333333333333333333333333333333*gt33L*trKL)*e4phi;
+    
+    CCTK_REAL alpL = alphaL;
+    
+    CCTK_REAL betaxL = beta1L;
+    
+    CCTK_REAL betayL = beta2L;
+    
+    CCTK_REAL betazL = beta3L;
+    
+    /* Copy local copies back to grid functions */
+    alp[index] = alpL;
+    betax[index] = betaxL;
+    betay[index] = betayL;
+    betaz[index] = betazL;
+    gxx[index] = gxxL;
+    gxy[index] = gxyL;
+    gxz[index] = gxzL;
+    gyy[index] = gyyL;
+    gyz[index] = gyzL;
+    gzz[index] = gzzL;
+    kxx[index] = kxxL;
+    kxy[index] = kxyL;
+    kxz[index] = kxzL;
+    kyy[index] = kyyL;
+    kyz[index] = kyzL;
+    kzz[index] = kzzL;
+  }
+  CCTK_ENDLOOP3(HOST_ML_BSSN_convertToADMBase);
+}
+
+extern "C" void HOST_ML_BSSN_convertToADMBase(CCTK_ARGUMENTS)
+{
+  DECLARE_CCTK_ARGUMENTS;
+  DECLARE_CCTK_PARAMETERS;
+  
+  
+  if (verbose > 1)
+  {
+    CCTK_VInfo(CCTK_THORNSTRING,"Entering HOST_ML_BSSN_convertToADMBase_Body");
+  }
+  
+  if (cctk_iteration % HOST_ML_BSSN_convertToADMBase_calc_every != HOST_ML_BSSN_convertToADMBase_calc_offset)
+  {
+    return;
+  }
+  
+  const char *const groups[] = {
+    "ADMBase::curv",
+    "ADMBase::lapse",
+    "ADMBase::metric",
+    "ADMBase::shift",
+    "ML_BSSN::ML_curv",
+    "ML_BSSN::ML_lapse",
+    "ML_BSSN::ML_log_confac",
+    "ML_BSSN::ML_metric",
+    "ML_BSSN::ML_shift",
+    "ML_BSSN::ML_trace_curv"};
+  GenericFD_AssertGroupStorage(cctkGH, "HOST_ML_BSSN_convertToADMBase", 10, groups);
+  
+  
+  GenericFD_LoopOverEverything(cctkGH, HOST_ML_BSSN_convertToADMBase_Body);
+  
+  if (verbose > 1)
+  {
+    CCTK_VInfo(CCTK_THORNSTRING,"Leaving HOST_ML_BSSN_convertToADMBase_Body");
+  }
+}
---
+++ ML_BSSN_Host/src/ML_BSSN_Host_convertToADMBase.cc
@@ -0,0 +1,224 @@
+/*  File produced by Kranc */
+
+#define KRANC_C
+
+#include <assert.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "cctk.h"
+#include "cctk_Arguments.h"
+#include "cctk_Parameters.h"
+#include "GenericFD.h"
+#include "Differencing.h"
+#include "cctk_Loop.h"
+#include "loopcontrol.h"
+#include "vectors.h"
+
+/* Define macros used in calculations */
+#define INITVALUE (42)
+#define QAD(x) (SQR(SQR(x)))
+#define INV(x) (kdiv(ToReal(1.0),x))
+#define SQR(x) (kmul(x,x))
+#define CUB(x) (kmul(x,SQR(x)))
+
+static void ML_BSSN_Host_convertToADMBase_Body(cGH const * restrict const cctkGH, int const dir, int const face, CCTK_REAL const normal[3], CCTK_REAL const tangentA[3], CCTK_REAL const tangentB[3], int const imin[3], int const imax[3], int const n_subblock_gfs, CCTK_REAL * restrict const subblock_gfs[])
+{
+  DECLARE_CCTK_ARGUMENTS;
+  DECLARE_CCTK_PARAMETERS;
+  
+  
+  /* Include user-supplied include files */
+  
+  /* Initialise finite differencing variables */
+  ptrdiff_t const di = 1;
+  ptrdiff_t const dj = CCTK_GFINDEX3D(cctkGH,0,1,0) - CCTK_GFINDEX3D(cctkGH,0,0,0);
+  ptrdiff_t const dk = CCTK_GFINDEX3D(cctkGH,0,0,1) - CCTK_GFINDEX3D(cctkGH,0,0,0);
+  ptrdiff_t const cdi = sizeof(CCTK_REAL) * di;
+  ptrdiff_t const cdj = sizeof(CCTK_REAL) * dj;
+  ptrdiff_t const cdk = sizeof(CCTK_REAL) * dk;
+  CCTK_REAL_VEC const dx = ToReal(CCTK_DELTA_SPACE(0));
+  CCTK_REAL_VEC const dy = ToReal(CCTK_DELTA_SPACE(1));
+  CCTK_REAL_VEC const dz = ToReal(CCTK_DELTA_SPACE(2));
+  CCTK_REAL_VEC const dt = ToReal(CCTK_DELTA_TIME);
+  CCTK_REAL_VEC const t = ToReal(cctk_time);
+  CCTK_REAL_VEC const dxi = INV(dx);
+  CCTK_REAL_VEC const dyi = INV(dy);
+  CCTK_REAL_VEC const dzi = INV(dz);
+  CCTK_REAL_VEC const khalf = ToReal(0.5);
+  CCTK_REAL_VEC const kthird = ToReal(1.0/3.0);
+  CCTK_REAL_VEC const ktwothird = ToReal(2.0/3.0);
+  CCTK_REAL_VEC const kfourthird = ToReal(4.0/3.0);
+  CCTK_REAL_VEC const keightthird = ToReal(8.0/3.0);
+  CCTK_REAL_VEC const hdxi = kmul(ToReal(0.5), dxi);
+  CCTK_REAL_VEC const hdyi = kmul(ToReal(0.5), dyi);
+  CCTK_REAL_VEC const hdzi = kmul(ToReal(0.5), dzi);
+  
+  /* Initialize predefined quantities */
+  CCTK_REAL_VEC const p1o1024dx = kmul(INV(dx),ToReal(0.0009765625));
+  CCTK_REAL_VEC const p1o1024dy = kmul(INV(dy),ToReal(0.0009765625));
+  CCTK_REAL_VEC const p1o1024dz = kmul(INV(dz),ToReal(0.0009765625));
+  CCTK_REAL_VEC const p1o1680dx = kmul(INV(dx),ToReal(0.000595238095238095238095238095238));
+  CCTK_REAL_VEC const p1o1680dy = kmul(INV(dy),ToReal(0.000595238095238095238095238095238));
+  CCTK_REAL_VEC const p1o1680dz = kmul(INV(dz),ToReal(0.000595238095238095238095238095238));
+  CCTK_REAL_VEC const p1o5040dx2 = kmul(INV(SQR(dx)),ToReal(0.000198412698412698412698412698413));
+  CCTK_REAL_VEC const p1o5040dy2 = kmul(INV(SQR(dy)),ToReal(0.000198412698412698412698412698413));
+  CCTK_REAL_VEC const p1o5040dz2 = kmul(INV(SQR(dz)),ToReal(0.000198412698412698412698412698413));
+  CCTK_REAL_VEC const p1o560dx = kmul(INV(dx),ToReal(0.00178571428571428571428571428571));
+  CCTK_REAL_VEC const p1o560dy = kmul(INV(dy),ToReal(0.00178571428571428571428571428571));
+  CCTK_REAL_VEC const p1o560dz = kmul(INV(dz),ToReal(0.00178571428571428571428571428571));
+  CCTK_REAL_VEC const p1o705600dxdy = kmul(INV(kmul(dx,dy)),ToReal(1.41723356009070294784580498866e-6));
+  CCTK_REAL_VEC const p1o705600dxdz = kmul(INV(kmul(dx,dz)),ToReal(1.41723356009070294784580498866e-6));
+  CCTK_REAL_VEC const p1o705600dydz = kmul(INV(kmul(dy,dz)),ToReal(1.41723356009070294784580498866e-6));
+  CCTK_REAL_VEC const p1o840dx = kmul(INV(dx),ToReal(0.00119047619047619047619047619048));
+  CCTK_REAL_VEC const p1o840dy = kmul(INV(dy),ToReal(0.00119047619047619047619047619048));
+  CCTK_REAL_VEC const p1o840dz = kmul(INV(dz),ToReal(0.00119047619047619047619047619048));
+  CCTK_REAL_VEC const p1odx = INV(dx);
+  CCTK_REAL_VEC const p1ody = INV(dy);
+  CCTK_REAL_VEC const p1odz = INV(dz);
+  
+  /* Assign local copies of arrays functions */
+  
+  
+  
+  /* Calculate temporaries and arrays functions */
+  
+  /* Copy local copies back to grid functions */
+  
+  /* Loop over the grid points */
+  #pragma omp parallel
+  LC_LOOP3VEC(ML_BSSN_Host_convertToADMBase,
+    i,j,k, imin[0],imin[1],imin[2], imax[0],imax[1],imax[2],
+    cctk_lsh[0],cctk_lsh[1],cctk_lsh[2],
+    CCTK_REAL_VEC_SIZE)
+  {
+    ptrdiff_t const index = di*i + dj*j + dk*k;
+    
+    /* Assign local copies of grid functions */
+    
+    CCTK_REAL_VEC alphaL = vec_load(alpha[index]);
+    CCTK_REAL_VEC At11L = vec_load(At11[index]);
+    CCTK_REAL_VEC At12L = vec_load(At12[index]);
+    CCTK_REAL_VEC At13L = vec_load(At13[index]);
+    CCTK_REAL_VEC At22L = vec_load(At22[index]);
+    CCTK_REAL_VEC At23L = vec_load(At23[index]);
+    CCTK_REAL_VEC At33L = vec_load(At33[index]);
+    CCTK_REAL_VEC beta1L = vec_load(beta1[index]);
+    CCTK_REAL_VEC beta2L = vec_load(beta2[index]);
+    CCTK_REAL_VEC beta3L = vec_load(beta3[index]);
+    CCTK_REAL_VEC gt11L = vec_load(gt11[index]);
+    CCTK_REAL_VEC gt12L = vec_load(gt12[index]);
+    CCTK_REAL_VEC gt13L = vec_load(gt13[index]);
+    CCTK_REAL_VEC gt22L = vec_load(gt22[index]);
+    CCTK_REAL_VEC gt23L = vec_load(gt23[index]);
+    CCTK_REAL_VEC gt33L = vec_load(gt33[index]);
+    CCTK_REAL_VEC phiL = vec_load(phi[index]);
+    CCTK_REAL_VEC trKL = vec_load(trK[index]);
+    
+    
+    /* Include user supplied include files */
+    
+    /* Precompute derivatives */
+    
+    /* Calculate temporaries and grid functions */
+    CCTK_REAL_VEC e4phi = 
+      IfThen(conformalMethod,INV(SQR(phiL)),kexp(kmul(phiL,ToReal(4))));
+    
+    CCTK_REAL_VEC gxxL = kmul(gt11L,e4phi);
+    
+    CCTK_REAL_VEC gxyL = kmul(gt12L,e4phi);
+    
+    CCTK_REAL_VEC gxzL = kmul(gt13L,e4phi);
+    
+    CCTK_REAL_VEC gyyL = kmul(gt22L,e4phi);
+    
+    CCTK_REAL_VEC gyzL = kmul(gt23L,e4phi);
+    
+    CCTK_REAL_VEC gzzL = kmul(gt33L,e4phi);
+    
+    CCTK_REAL_VEC kxxL = 
+      kmul(e4phi,kmadd(gt11L,kmul(trKL,ToReal(0.333333333333333333333333333333)),At11L));
+    
+    CCTK_REAL_VEC kxyL = 
+      kmul(e4phi,kmadd(gt12L,kmul(trKL,ToReal(0.333333333333333333333333333333)),At12L));
+    
+    CCTK_REAL_VEC kxzL = 
+      kmul(e4phi,kmadd(gt13L,kmul(trKL,ToReal(0.333333333333333333333333333333)),At13L));
+    
+    CCTK_REAL_VEC kyyL = 
+      kmul(e4phi,kmadd(gt22L,kmul(trKL,ToReal(0.333333333333333333333333333333)),At22L));
+    
+    CCTK_REAL_VEC kyzL = 
+      kmul(e4phi,kmadd(gt23L,kmul(trKL,ToReal(0.333333333333333333333333333333)),At23L));
+    
+    CCTK_REAL_VEC kzzL = 
+      kmul(e4phi,kmadd(gt33L,kmul(trKL,ToReal(0.333333333333333333333333333333)),At33L));
+    
+    CCTK_REAL_VEC alpL = alphaL;
+    
+    CCTK_REAL_VEC betaxL = beta1L;
+    
+    CCTK_REAL_VEC betayL = beta2L;
+    
+    CCTK_REAL_VEC betazL = beta3L;
+    
+    /* Copy local copies back to grid functions */
+    vec_store_partial_prepare(i,lc_imin,lc_imax);
+    vec_store_nta_partial(alp[index],alpL);
+    vec_store_nta_partial(betax[index],betaxL);
+    vec_store_nta_partial(betay[index],betayL);
+    vec_store_nta_partial(betaz[index],betazL);
+    vec_store_nta_partial(gxx[index],gxxL);
+    vec_store_nta_partial(gxy[index],gxyL);
+    vec_store_nta_partial(gxz[index],gxzL);
+    vec_store_nta_partial(gyy[index],gyyL);
+    vec_store_nta_partial(gyz[index],gyzL);
+    vec_store_nta_partial(gzz[index],gzzL);
+    vec_store_nta_partial(kxx[index],kxxL);
+    vec_store_nta_partial(kxy[index],kxyL);
+    vec_store_nta_partial(kxz[index],kxzL);
+    vec_store_nta_partial(kyy[index],kyyL);
+    vec_store_nta_partial(kyz[index],kyzL);
+    vec_store_nta_partial(kzz[index],kzzL);
+  }
+  LC_ENDLOOP3VEC(ML_BSSN_Host_convertToADMBase);
+}
+
+extern "C" void ML_BSSN_Host_convertToADMBase(CCTK_ARGUMENTS)
+{
+  DECLARE_CCTK_ARGUMENTS;
+  DECLARE_CCTK_PARAMETERS;
+  
+  
+  if (verbose > 1)
+  {
+    CCTK_VInfo(CCTK_THORNSTRING,"Entering ML_BSSN_Host_convertToADMBase_Body");
+  }
+  
+  if (cctk_iteration % ML_BSSN_Host_convertToADMBase_calc_every != ML_BSSN_Host_convertToADMBase_calc_offset)
+  {
+    return;
+  }
+  
+  const char *const groups[] = {
+    "ADMBase::curv",
+    "ADMBase::lapse",
+    "ADMBase::metric",
+    "ADMBase::shift",
+    "ML_BSSN_Host::ML_curv",
+    "ML_BSSN_Host::ML_lapse",
+    "ML_BSSN_Host::ML_log_confac",
+    "ML_BSSN_Host::ML_metric",
+    "ML_BSSN_Host::ML_shift",
+    "ML_BSSN_Host::ML_trace_curv"};
+  GenericFD_AssertGroupStorage(cctkGH, "ML_BSSN_Host_convertToADMBase", 10, groups);
+  
+  
+  GenericFD_LoopOverEverything(cctkGH, ML_BSSN_Host_convertToADMBase_Body);
+  
+  if (verbose > 1)
+  {
+    CCTK_VInfo(CCTK_THORNSTRING,"Leaving ML_BSSN_Host_convertToADMBase_Body");
+  }
+}
